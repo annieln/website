6f968b0b5f83c10ef5983d56be874ad236eee935
annie <annieyhlin09@gmail.com>
2025-03-07 00:15:14 -0500

    resolve apostrophe escape issue

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center text-center py-36">
-      <p className="mb-8 max-w-md font-serif">how'd ya even get here?</p>
+      <p className="mb-8 max-w-md font-serif">{`how'd ya even get here?`}</p>
       <Link 
         href="/" 
         className="px-6 py-3 bg-[#1F2421] text-(--foreground) rounded-full hover:bg-(--secondary) hover:text-[#1F2421] transition-colors font-serif"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,12 @@ export default function Home() {
           <Profile />
         </div>
         <div className="space-y-6 font-serif font-stretch-ultra-expanded font-thin tracking-wider text-sm">
-          <p>Welcome to my digital playground... ♪</p>
-          <p>I'm a software engineer, media technologist, and computational artist living in my lovely hometown of Queens, NYC.</p>
-          <p>Currently, I'm building tools for safer digital interactions at Validia. Previously, I graduated from UC Berkeley with a dual degree in Applied Mathematics and Computer Science.</p>
-          <p>In my free time, I'm exploring computing tools for creative expression at the Design Tools Lab at Barnard College. My passions lie in the intersection of visual storytelling, post-digital intimacy, and open source technology.</p>
+          <p>{`Welcome to my digital playground... ♪`}</p>
+          <p>{`I'm a software engineer, media technologist, and computational artist living in my lovely hometown of Queens, NYC.`}</p>
+          <p>{`Currently, I'm building tools for safer digital interactions at Validia. Previously, I graduated from UC Berkeley with a dual degree in Applied Mathematics and Computer Science.`}</p>
+          <p>{`In my free time, I'm exploring computing tools for creative expression at the Design Tools Lab at Barnard College. My passions lie in the intersection of visual storytelling, post-digital intimacy, and open source technology.`}</p>
           ✦
-          <p className="italic font-serif text-xs pt-6">Thanks for stopping by! This site is very much a work in progress but I really wanted my web home to be live.<br/><br/>Check back in soon, I promise there will be more to see!</p>
+          <p className="italic font-serif text-xs pt-6">{`Thanks for stopping by! This site is very much a work in progress but I really wanted my web home to be live.`}<br/><br/>{`Check back in soon, I promise there will be more to see!`}</p>
         </div>
       </div>
     </>
